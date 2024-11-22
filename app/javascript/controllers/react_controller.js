4f25dc18f270cb19be61f2d445b6ee9dbc5af1a4
lucasilliano <lucas.illiano@hotmail.com>
2024-11-22 11:29:45 +0100

    first commit with tailwind, react setup

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import React from 'react';
+import App from '../components/App';
+import { createRoot } from 'react-dom/client';
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  static targets = ['root'];
+  connect() {
+    console.log("Hello, Stimulus!");
+    const app = document.getElementById('app');
+
+    const root = createRoot(app);
+    root.render(<App />);
+  }
+}
