183c5bf38dd5e73ab2859b964b073c9bd5693141
lucasilliano <lucas.illiano@hotmail.com>
2024-11-26 15:49:00 +0100

    adding json data and updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,39 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+require 'benchmark'
+
+time_take_destroy = Benchmark.measure do
+  Recipe.in_batches(of: 500).delete_all
+end
+puts "Time taken to destroy all records: #{time_take_destroy.real.round(2)} seconds"
+
+json_data = File.read('db/recipes-en.json')
+recipes = JSON.parse(json_data)
+
+# Batch size for optimized insertion
+BATCH_SIZE = 500
+
+# Measure time taken
+time_taken = Benchmark.measure do
+  recipes.each_slice(BATCH_SIZE) do |batch|
+    bulk_data = batch.map do |recipe|
+      {
+        title: recipe['title'],
+        cook_time: recipe['cook_time'],
+        prep_time: recipe['prep_time'],
+        ingredients: recipe['ingredients'].join(', '),
+        ratings: recipe['ratings'],
+        category: recipe['category'],
+        author: recipe['author'],
+        image_url: recipe['image'],
+        created_at: Time.current,
+        updated_at: Time.current
+      }
+    end
+
+    # Bulk insert for better performance
+    Recipe.insert_all(bulk_data)
+  end
+end
+
+puts "Database seeded successfully with #{recipes.size} records."
+puts "Time taken to create all records: #{time_taken.real.round(2)} seconds"
